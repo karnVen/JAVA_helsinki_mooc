d050763a15b0d019f6e2faedcae6c6ecbe60522f
karnVen <onemonth06@gmail.com>
2026-02-23 07:50:54 +0530

    feat: P01_09.Conversation

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -5,8 +5,11 @@ public class Conversation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Greetings! How are you doing?");
+        String reply=scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String reply2=scanner.nextLine();
+        System.out.println("Thanks for sharing!");
         // Write your program here
-
     }
 }
