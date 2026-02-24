90120d4e1dd77a37b4a1e805bd6d5d5197e89c22
karnVen <onemonth06@gmail.com>
2026-02-24 22:17:16 +0530

    feat: P01_12.integerinput

--- part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -5,6 +5,9 @@ public class IntegerInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number "+number);
 
         // write your program here
 
