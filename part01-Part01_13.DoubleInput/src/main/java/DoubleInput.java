ebbe9e77b429c52c36c05fb416ef5cb7ce5b252a
karnVen <onemonth06@gmail.com>
2026-02-25 23:29:40 +0530

    feat: P01_13.doubleInput

--- part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -5,6 +5,9 @@ public class DoubleInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        double number= Double.valueOf(scanner.nextLine());
+        System.out.println("You gave the number "+number);
 
         // write your program here
 
