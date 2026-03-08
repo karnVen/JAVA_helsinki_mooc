0d3328ebf1ff2ac2d16986931b1ba2dd7491ed1d
karnVen <onemonth06@gmail.com>
2026-03-08 17:30:49 +0530

    feat: till 26

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 public class Orwell {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int speed = Integer.valueOf(scanner.nextLine());
+        System.out.println("lets write some more to ");
+        int letfixsoemcool= Integer.valueOf((scanner.nextLine()));
+        System.out.println("neither more upddate is inouig ");
+
 
         // Write your program here
         System.out.println("Give a number: ");
         int num=Integer.valueOf(scan.nextLine());
         if(num==1984){
             System.out.println("Orwell");
+            System.out.println("just flexing out front of nefue ");
+            
         }
     }
 }
