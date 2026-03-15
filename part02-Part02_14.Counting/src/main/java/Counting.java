03c63d522962bda358b8c91bd170ca6b8f357724
karnVen <onemonth06@gmail.com>
2026-03-15 17:10:12 +0530

    ...

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,10 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        for(int i =0;i<=a;i++){
+            System.out.println(i);
+        }
 
     }
 }
--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -5,6 +5,12 @@ public class FromWhereToWhere {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Where to?");
+        int a = scanner.nextInt();
+        int b =scanner.nextInt();
+        for(int i =b;i<=a;i++){
+            System.out.println(i);
+        }
 
         // Write your program here
     }
